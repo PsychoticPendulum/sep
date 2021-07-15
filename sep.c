2b78e205e6c6bbbc66db64a510601edaf425455f
PsychoticPendulum <80325726+PsychoticPendulum@users.noreply.github.com>
2021-07-15 10:53:23 +0200

    Now with arguments
    Added a whole getOpt() part.

--- sep.c
+++ sep.c
@@ -1,7 +1,18 @@
+// |-------------------------------------------------------------------------|
+// |  ____                 _     _      ____                        _        |
+// | |  _ \ ___ _   _  ___| |__ (_) ___|  _ \ ___ _ __   __ _ _   _(_)_ __   |
+// | | |_) / __| | | |/ __| '_ \| |/ __| |_) / _ \ '_ \ / _` | | | | | '_ \  |
+// | |  __/\__ \ |_| | (__| | | | | (__|  __/  __/ | | | (_| | |_| | | | | | |
+// | |_|   |___/\__, |\___|_| |_|_|\___|_|   \___|_| |_|\__, |\__,_|_|_| |_| |
+// |           |___/                                   |___/                 |
+// |-------------------------------------------------------------------------| 
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 int getWidth(int len) {
 	struct winsize w;
@@ -9,20 +20,30 @@ int getWidth(int len) {
 	return w.ws_col / len;
 }
 
-void printSep(const char *symbol, int len) {
-	for (int i = 0; i < getWidth(len); i++) {
+void printSep(int len, int lines, const char *symbol) {
+	for (int i = 0; i < getWidth(len) * lines; i++) {
 		printf(symbol);
 	}
 	printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-	int len;
-	if (argc > 1) {
-		printSep(argv[1], strlen(argv[1]));
+	int option;
+	char *symbol = "+";
+	int lines = 1;
+
+	while ((option = getopt(argc, argv, "l:s:")) != -1) {
+		switch (option) {
+			case 'l':
+				lines = atoi(optarg);
+				break;
+			case 's':
+				symbol = optarg;
+				break;
+		}
 	}
-	else {
-		printSep("-", 1);
-	}
+	
+	printSep(strlen(symbol), lines, symbol);
 	return 0;
 }
+
